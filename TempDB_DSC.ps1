4521b8112d6b4bcbef6fa12c1cc3bad19d150ff1
jdanton <jdanton1@yahoo.com>
2023-02-17 15:53:06 -0500

    Update TempDB_DSC.ps1

--- TempDB_DSC.ps1
+++ TempDB_DSC.ps1
@@ -27,18 +27,4 @@ if (!(test-path -Path `$tempFolder)) {new-item -ItemType Directory -Path `$tempF
 start-service `$SQLServer
 start-service `$SQLAgentService"
 
-# Set Service to Manual Start
-
-set-service -name "MSSQLSERVER" -StartupType Manual
-
-
-#Scheduled Task Creation
-
-$a = New-ScheduledTaskAction -Execute "powershell.exe" -Argument C:\Scripts\TempDB.ps1
-$t = New-ScheduledTaskTrigger -AtStartup
-$p = New-ScheduledTaskPrincipal "NT Authority\SYSTEM"
-$s = New-ScheduledTaskSettingsSet
-$d = New-ScheduledTask -Action $a -Principal $p -Trigger $t -Settings $s
-Register-ScheduledTask TempDBJob -InputObject $d
-
 
